24059027955327bc67835dd781f97ae3e4ffab6d
ramadevadiga <68851934+ramadevadiga@users.noreply.github.com>
2020-07-28 15:33:24 +0530

    function split

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,21 @@
-
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  int i;
+bool isBpmOK(float bpm) {  
   if(bpm < 70 || bpm > 150) {
     return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
+  } 
+  return true;
+}
+bool isSpoOK(float spo2) {  
+  if(spo2 < 80) {
     return false;
   }
   return true;
+}
+bool isRespRateOK(float respRate) {  
+  if(respRate < 30 || respRate > 60) {
+    return false;
+  }
+  return true;
+}
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+  return isBpmOK(bpm) &&  isSpoOK(spo2) && isRespRateOK(respRate);
 }
